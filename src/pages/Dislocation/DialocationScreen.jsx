7f520fa387c59adb1eb9657dca108406daec3000
DoniyorShuxratov <shuxratovdoni@gmail.com>
2023-12-26 20:32:05 +0500

    update

--- src/pages/Dislocation/DialocationScreen.jsx
+++ src/pages/Dislocation/DialocationScreen.jsx
@@ -1,8 +1,14 @@
 import { useLocation } from "react-router-dom"
-import data from '../../data/data.json'
+import { useState } from "react";
 
 export default function DislocatioScreen() {
     const {state: dislocation} = useLocation();
+    const [selectedProduct, setSelectedProduct] = useState(null);
+
+    const handleProductClick = (productType) => {
+        setSelectedProduct(productType);
+    }
+
     return(
         <section className="locatescreen--section">
             <div className="locatescreen--section-container container">
@@ -12,7 +18,41 @@ export default function DislocatioScreen() {
                     <p><b>ОРТ: </b>{dislocation.subInfo.landmark}</p>
                     <p><b>Время работы: </b>{dislocation.subInfo.workingTime}</p>
                 </div>
-                
+                <div className="locatescreen--contant">
+                    <div className="locatescreen--contant-catalog">
+                        {dislocation.categories.map((category) => (
+                            <button key={category.typeOfProduct} onClick={() => handleProductClick(category.typeOfProduct)} className={selectedProduct === category.typeOfProduct ? "selected" : ""}>
+                                {category.typeOfProduct}
+                            </button>
+                        ))}
+                    </div>
+                    <div className="locatescreen--contant-workers">
+                        {selectedProduct && (
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Type of Work</th>
+                                        <th>Project</th>
+                                        <th>Phone</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {dislocation.categories
+                                   .find((category) => category.typeOfProduct === selectedProduct)
+                                   .listOfWorkers.map((worker, index) => (
+                                        <tr key={index}>
+                                            <td>{worker.name}</td>
+                                            <td>{worker.typeWork}</td>
+                                            <td>{worker.project}</td>
+                                            <td>{worker.phone}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        )}
+                    </div>
+                </div>
             </div>
         </section>
     )
